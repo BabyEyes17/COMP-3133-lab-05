61460385ad4a378a61d53192e682f0bb54b89663
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-22 21:30:52 -0500

    Completed schema based off Movie model

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,58 @@
 import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+
+    type Movie {
+    
+        name: String!
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: Float!
+    }
+
+
+
+    input AddMovieInput {
+    
+        name: String!
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: Float!
+    }
+
+
+
+    input UpdateMovieInput {
+    
+        name: String
+        director_name: String
+        production_house: String
+        release_date: String
+        rating: Float
+    }
+
+
+    type Query {
+    
+        getAllMovies: [Movie!]!
+
+        searchMovieById(id: ID!): Movie
+
+        searchMovieByDirectorName(director_name: String): [Movie]
+    }
+
+
+
+    type Mutation {
+    
+        addMovie(input: AddMovieInput!): Movie!
+
+        updateMovieById(id: ID!, input: UpdateMovieInput!): Movie!
+
+        deleteMovieById(id: ID!): Movie!
+    }
+`;
+
+module.exports = typeDefs;
