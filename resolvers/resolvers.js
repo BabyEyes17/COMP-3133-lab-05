927028dcd4b4d3c59c5b3163c4a215bf42fa438f
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-22 21:41:40 -0500

    Completed movie queries

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,38 @@
 import MovieModel from '../models/movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
+
+const resolvers = {
+
+    Query: {
+
+        getAllMovies: async() => {
+
+            return await Movie.find().sort({ release_date: -1});
+        },
+
+        searchMovieById: async (_, { id }) => {
+
+            return await Movie.findById(id);
+        },
+
+        searchMovieByDirectorName: async (_, { director_name }) => {
+
+            return await Movie.find({ director_name }).sort({ release_date: -1 });
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
